a5a57afce775cf103ad93524660ef6980f1f187c
haupadhy <Ganeshd0yourbest>
2019-10-21 14:19:01 -0400

    Updated script

--- project/Azure_db.py
+++ project/Azure_db.py
@@ -7,7 +7,7 @@ password = 'Ganeshd0yourbest'
 driver= '{ODBC Driver 17 for SQL Server}'
 
 
-cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+con = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 
 cur = con.cursor()
 string = "CREATE TABLE tst(name varchar(15), id integer)"
