a934711c18bf242b6f98676e277576388c8956fd
haupadhy <Ganeshd0yourbest>
2019-10-21 11:07:45 -0400

    test connection

---
+++ project/Azure_db.py
@@ -0,0 +1,15 @@
+import pyodbc
+
+server = 'abssrv.database.windows.net'
+database = 'absdb'
+username = 'haupadhy'
+password = 'Ganeshd0yourbest'
+driver= '{ODBC Driver 17 for SQL Server}'
+
+
+cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+
+cur = con.cursor()
+string = "CREATE TABLE tst(name varchar(15), id integer)"
+cur.execute(string)
+con.commit()
