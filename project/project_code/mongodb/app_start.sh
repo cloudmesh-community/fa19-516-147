93c499e4ac5e62b2bedc6b18f06d5e75e081f377
haupadhy <Ganeshd0yourbest>
2020-05-04 22:46:37 -0400

    app start

--- project/project_code/mongodb/app_start.sh
+++ project/project_code/mongodb/app_start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-cd ./
+
 DATE=`date '+%Y-%m-%d %H%M%S'`
 DATE=$(echo "$DATE" | tr ' ' _)
 #log_file_name= ~/server_$DATE.log
-sudo service mongodb start
 source ~/ENV3/bin/activate
-python mongodb/server.py
+python server.py
+#open http://127.0.0.1:8080/cloudmesh/v3/ui/
 #> $log_file_name
 #deactivate
-
